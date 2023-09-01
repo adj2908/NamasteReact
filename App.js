f05af6adeeda2b87ee4fa806649fdae7f64fdbf3
Aditya Jain <adityajain@Adityas-MacBook-Air-2.local>
2023-09-01 18:08:01 +0530

    ep2-added npm and node_module

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
-const header=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[React.createElement("h2",{id:"child2"},"I am header2")]),
-    React.createElement("div",{id:"child3"},[React.createElement("h2",{id:"child4"},"I am header2")]),
-]);
+import React from 'react';
+import ReactDOM from'react-dom/client';
+const parent=React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+  ]);
 const reactRoot=ReactDOM.createRoot(document.getElementById("root"));
-reactRoot.render(header);
+reactRoot.render(parent);
